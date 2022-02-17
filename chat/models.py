32f4b6ccf3dc636987b72179fa0b67a8e97e928e
okeowo clinton <okeowo1014@gmail.com>
2022-02-17 18:20:39 +0100

    add runtime

--- chat/models.py
+++ chat/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.contrib.auth import get_user_model
 from django.db import models
 # Create your models here.
 from api.models import User
@@ -27,11 +26,6 @@ class ChatMessage(models.Model):
     message_type = models.CharField(max_length=10, choices=MSG_TYPE)
     channel = models.ForeignKey(ChatChannels, on_delete=models.CASCADE, related_name='chat_channel')
     created = models.DateTimeField(auto_now_add=True)
-
-
-# Create your models here.
-User = get_user_model()
-
 
 class DMChatMessage(models.Model):
     chatid = models.CharField(max_length=255)
