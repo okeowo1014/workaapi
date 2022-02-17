68685c378b480df0b3ac5a8a12801b6d08142f4b
okeowo clinton <okeowo1014@gmail.com>
2022-02-17 15:14:20 +0100

    add runtime

--- workaapi/asgi.py
+++ workaapi/asgi.py
@@ -19,7 +19,6 @@ from django.core.asgi import get_asgi_application
 import chat.routing
 
 application = ProtocolTypeRouter({
-    "https": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
