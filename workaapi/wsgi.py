bce544c818d892a076ae5b353d8fa6d18ded1680
okeowo clinton <okeowo1014@gmail.com>
2022-02-17 15:10:35 +0100

    add runtime

--- workaapi/wsgi.py
+++ workaapi/wsgi.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 
 import os
 
+import gunicorn as gunicorn
 from django.core.wsgi import get_wsgi_application
+t=gunicorn
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workaapi.settings')
 
