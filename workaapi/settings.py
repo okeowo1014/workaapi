8504d9ad269be3f704e71d85254c7345d93339bd
okeowo clinton <okeowo1014@gmail.com>
2022-06-03 17:56:41 +0100

    add runtime

--- workaapi/settings.py
+++ workaapi/settings.py
@@ -37,7 +37,7 @@ if production:
     tunnel = sshtunnel.SSHTunnelForwarder(
         (sshhost),
         ssh_username=sshusername, ssh_password=sshpassword,
-        remote_bind_address=(sshaddress, sshport)
+        remote_bind_address=(sshaddress, int(sshport))
     )
     tunnel.start()
 
